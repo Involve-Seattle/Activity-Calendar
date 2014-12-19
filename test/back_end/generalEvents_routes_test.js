9c88ca109e0ea555eafd40ba49be3b4ac7ba4a9f
Jacob Shafer <jacshfr@gmail.com>
2014-12-18 20:20:27 -0800

    SQASHED!

--- test/back_end/generalEvents_routes_test.js
+++ test/back_end/generalEvents_routes_test.js
@@ -3,15 +3,19 @@
 var chai = require('chai');
 var chaihttp = require('chai-http');
 var GeneralEvents = require('../../models/generalEvents');
+var User = require('../../models/user');
 chai.use(chaihttp);
 
 require('../../server');
 
 var expect = chai.expect;
 
+var email = new Buffer('test@example.com', 'ascii').toString('base64');
+var password = new Buffer('Test1@', 'ascii').toString('base64');
+var authorization = 'Basic ' + new Buffer('test@example.com:Test1@', 'ascii').toString('base64');
 var token;
 
-before(function() {
+before(function(done) {
   GeneralEvents.remove({}, function(err) {
     if (err) return console.log(err);
     console.log('db cleaned');
@@ -23,6 +27,7 @@ before(function() {
   newEvent.eventAddress = 'test address';
   newEvent.save(function(err, data) {
   });
+  done();
 });
 
 after(function() {
@@ -30,14 +35,33 @@ after(function() {
     if (err) return console.log(err);
     console.log('db cleaned');
   });
+
+  User.remove({}, function(err) {
+    console.log('users cleaned');
+  });
 });
 
 describe('generalEvents request', function() {
 
+  it('should create a user', function(done) {
+    chai.request('http://localhost:3000')
+    .post('/api/newUser')
+    .send({
+      email: email,
+      password: password,
+      passwordConfirmation: password,
+      locations: 'testCity'
+    })
+    .end(function(err, res) {
+      token = res.body.jwt;
+      done();
+    });
+  });
+
   it('should pull events from db', function(done) {
     chai.request('http://localhost:3000')
     .get('/api/events')
-    // .set({jwt:token})
+    .set({jwt:token})
     .end(function(err, res) {
       expect(err).to.eql(null);
       expect(res.body[0]).to.have.property('eventTitle');
