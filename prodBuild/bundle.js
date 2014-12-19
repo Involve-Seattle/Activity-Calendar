c8f190b091831bae7ce189384fcb68b170f447b6
Chareesa <chareesagraham@gmail.com>
2014-12-19 01:29:22 -0800

    heroku deploy

--- prodBuild/bundle.js
+++ prodBuild/bundle.js
@@ -77,16 +77,20 @@ module.exports = function(app) {
       templateUrl: '/templates/directives/header_template.html',
 
       controller: function($scope, $location, userService) {
+        $scope.showMenu = false;
 
         $scope.calendar = function() {
+          $scope.showMenu = false;
           $location.path('/calendar');
         };
 
         $scope.mymeetings = function() {
+          $scope.showMenu = false;
           $location.path('/mymeetings');
         };
 
         $scope.logout = function() {
+          $scope.showMenu = false;
           $location.path('/login');
           userService.logout();
         };
