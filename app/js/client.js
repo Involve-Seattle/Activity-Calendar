2ef750868c7659949de768f0f32fe73b22ced19a
Stephanie Lingwood <stephanielingwood@gmail.com>
2014-12-18 17:41:23 -0800

    moved header to a directive and fixed header bugs

--- app/js/client.js
+++ app/js/client.js
@@ -6,15 +6,18 @@ require('angular-base64');
 
 var involveApp = angular.module('involveApp', ['ngRoute', 'ngCookies', 'base64']);
 
+//directives
+require('./directives/header_directive.js')(involveApp);
+
 //services
-require('./events/services/resource_backend_service')(involveApp);
 require('./user/services/user_service')(involveApp);
+require('./events/services/resource_backend_service')(involveApp);
 
 //controllers
-require('./events/controllers/calendar_controller.js')(involveApp);
-// <<<<<<< HEAD
-require('./controllers/nav_controller.js')(involveApp);
+require('./controllers/index_controller.js')(involveApp);
 require('./user/controllers/login_controller.js')(involveApp);
+require('./events/controllers/calendar_controller.js')(involveApp);
+require('./mymeetings/controllers/mymeetings_controller.js')(involveApp);
 
 involveApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider
@@ -29,6 +32,10 @@ involveApp.config(['$routeProvider', function($routeProvider) {
   .when('/singleView', {
     templateUrl: 'templates/events/event_template.html',
     controller: 'calendCtrl'
+  })
+  .when('/mymeetings', {
+    templateUrl: 'templates/mymeetings/mymeetings_template.html',
+    controller: 'mymeetCtrl'
   })
   .otherwise({
     redirectTo: '/login'
--- app/js/user/services/user_service.js
+++ app/js/user/services/user_service.js
@@ -15,13 +15,6 @@ module.exports = function(app) {
       },
 
       signUp: function(newUser) {
-        if (newUser.password !== newUser.passwordConfirmation || newUser.password === undefined) {
-          return ({msg: 'password and confirmation did not match'});
-        }
-        if (!newUser.email) {
-          return ({msg: 'did not specify an email'});
-        }
-
         var newUserEncoded = {};
         newUserEncoded.email = $base64.encode(newUser.email);
         newUserEncoded.password = $base64.encode(newUser.password);
@@ -48,17 +41,17 @@ module.exports = function(app) {
       signedIn: function($cookies) {
         if (!$cookies.jwt || !$cookies.jwt.length) return $location.path('/login');
         // console.log('user logged in');
-        $location.path('/calendar');
+        // $location.path('/calendar');
       },
 
       logout: function() {
-        delete $cookies.jwt;
+        $cookies.jwt = undefined;
         $rootScope.user = {
           email: null,
           loggedin: false
         };
         $rootScope.$broadcast('user:loggedOut');
-        return $location.path('/login');
+        return $cookies;
       }
 
     };
